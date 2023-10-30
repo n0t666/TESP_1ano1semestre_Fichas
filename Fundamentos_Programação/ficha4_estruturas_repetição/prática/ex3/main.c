60764bc613acde7c60d291f2f97b71265442b5e0
Leandro Pereira <opereira3591@gmail.com>
2023-10-30 21:46:11 +0000

    Inserted new ex

--- Fundamentos_Programação/ficha4_estruturas_repetição/prática/ex3/main.c
+++ Fundamentos_Programação/ficha4_estruturas_repetição/prática/ex3/main.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void main(void)
+#include <locale.h>
+int main()
 {
-    int atleta;
-    float maior=0,menor=0,altura=0,media;
+    int total_saltos = 0;
+    float salto = 0,maior_salto = 0,menor_salto=0,soma_saltos = 0,media = 0;
 
-    while(altura != 0)
+
+    printf("Introduza o comprimento do salto (0 para terminar): ");
+
+    while (scanf("%f", &salto) == 1 && salto != 0.0)
     {
-        if (altura > maior)
+        if (salto > 0.0)
         {
-            maior=altura;
-        }
-        else
-        {
-            menor= altura;
-        }
+            total_saltos++;
+            soma_saltos = soma_saltos + salto;
 
+            if (total_saltos == 1)
+            {
+                maior_salto = menor_salto = salto;
+            }
+            else
+            {
+                if (salto > maior_salto)
+                {
+                    maior_salto = salto;
+                }
+                if (salto < menor_salto)
+                {
+                    menor_salto = salto;
+                }
+            }
+        }
+        printf("Introduza o comprimento do salto (0 para terminar): ");
     }
 
+    if (total_saltos > 0)
+    {
+        media = soma_saltos / total_saltos;
+        printf("Número total de saltos: %d\n", total_saltos);
+        printf("Maior comprimento de salto: %.2f\n", maior_salto);
+        printf("Menor comprimento de salto: %.2f\n", menor_salto);
+        printf("Média de comprimentos de salto: %.2f\n", media);
+    }
+    else
+    {
+        printf("Nenhum salto válido foi introduzido.\n");
+    }
 
-
-
-    printf("%f %f",maior,menor);
     return 0;
 }
